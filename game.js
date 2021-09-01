e5d86c500532b98ad4dda36baff3f0766c4e788d
Adrika-Singhal <77188343+Adrika-Singhal@users.noreply.github.com>
2021-09-01 18:18:30 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -28,3 +28,62 @@ document.getElementById("output").innerHTML = row;
 document.getElementById("math1").value = "";
 document.getElementById("math2").value = "";
 }
+
+player1_name = localStorage.getItem("player1");
+player2_name = localStorage.getItem("player2");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+
+document.getElementById("player_question").innerHTML = "Question Turn -" + player1_name ;
+document.getElementById("player_answer").innerHTML = "Answer Turn -" + player2_name ;
+
+function check()
+{
+ get_answer = document.getElementById("input_check_box").value;
+ if(answer == number)
+ {
+     if(answer_turn == "player1")
+     {
+         player1_score = player1_score +1;
+         document.getElementById("player1_score").innerHTML = player1_score;
+
+     }
+     else
+     {
+         player2_score = player2_score +1;
+         document.getElementById("player2_score").innerHTML = player2_score;
+        
+     }
+ }
+ if(question_turn == "player1")
+ {
+question_turn = "player2";
+document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name ;
+
+ }
+ else 
+ {
+ question_turn = "player1";
+ document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+ }
+ if (answer_turn == "player1")
+  {
+   answer_turn = "player2";
+   document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+ 
+ }
+ else  {
+    answer_turn = "player1";
+    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
+  
+  }
+  document.getElementById("output").innerHTML = "";
+}
